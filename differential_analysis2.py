c8081f383b6ec4e3ad064f89f55520439a504c9d
Ritobroto Mohanta <ritobrotom@examroom.ai>
2024-09-10 12:31:33 +0530

    DA2 description added

--- differential_analysis2.py
+++ differential_analysis2.py
@@ -8,3 +8,34 @@ import os
 plt.style.use('bmh') 
 file_path = './reports/differential_analysis2/'
 createFolder(file_path)
+
+ 
+# Differential equation of interest
+def super_func(k=1):
+    def wrapper(x,y):
+        return k*np.cos(y)
+    return wrapper
+
+
+x = np.linspace(-1,8,200)
+# y_init = np.pi/2              # initial condition
+
+
+
+plt.figure()
+for y_init in [-0.5*np.pi,-0.7*np.pi,-0.3*np.pi,0,0.3*np.pi,1.2*np.pi,1.7*np.pi]:
+    inside_func = super_func()
+    y_val = dyff_eqn_soln(inside_func,x,y_init)
+    plt.plot(x,y_val/np.pi,label=f"y_init={y_init:.1f}")
+plt.ylabel("y")
+plt.xlabel("x/pi")
+plt.title("Differential equation: y'=k*cos(y); k=1",weight='bold')
+plt.axhline(y = -0.5, color = 'r', linestyle = ':',label="zeros of cos(y)") 
+plt.axhline(y = 1.5, color = 'r', linestyle = ':') 
+plt.axhline(y = 2.5, color = 'r', linestyle = ':') 
+plt.axhline(y = 0.5, color = 'r', linestyle = ':') 
+plt.axhline(y = -1.5, color = 'r', linestyle = ':') 
+plt.legend()
+plt.savefig(file_path + 'foo.png', bbox_inches='tight')
+plt.show()
+
---
+++ reports/differential_analysis2/description.md
@@ -0,0 +1,11 @@
+# Behaviour of simple FODE with varying initial conditions
+Consider the following first order differential equation-
+$$ \frac{dy}{dx} = f(x,y) $$
+
+Say we take $f(x,y)$ in the form of $f(y)$, where the function has only output dependence.
+In this section we have analysed how the the differenrtial behaves with varying initial conditions, and tried find out the patterns.
+
+## We have taken the following differenential equations:
+1. $ \frac{dy}{dx} = \sin(y) $
+2. $ \frac{dy}{dx} = \cos(y) $
+3. $ \frac{dy}{dx} = (x^2-1) $
